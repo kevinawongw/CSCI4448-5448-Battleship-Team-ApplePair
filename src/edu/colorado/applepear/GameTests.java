a468f131576b0172b9295db9e26d022adf6ab173
vonvonliu <yuli4421@colorado.edu>
2021-02-13 21:51:33 -0700

    Added a class for our JUnit Tests!

---
+++ src/edu/colorado/applepear/GameTests.java
@@ -0,0 +1,16 @@
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAsser.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class GameTest{
+    Private Game game;
+
+    @Before
+    public void setUp(){
+        game = new Game();
+    }
+
+    @Test
+}
