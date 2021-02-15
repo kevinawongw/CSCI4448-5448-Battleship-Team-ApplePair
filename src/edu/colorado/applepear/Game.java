2ce13271e9c672596fe3493d9c9af2a06149fd17
Yubin Go(Sally) <54964332+yugo9081@users.noreply.github.com>
2021-02-16 08:02:24 +0900

    Team applepear pair 2: created six functions in Game class

--- src/edu/colorado/applepear/Game.java
+++ src/edu/colorado/applepear/Game.java
@@ -2,14 +2,28 @@ package edu.colorado.applepear;
 //Yubin part
 public class Game {
     //1: keeps track of and displays the winner
+    public void winner(){
 
+    }
     //2: determines which player's turn it is
+    public void determinateTurn(){
 
+    }
     //3: creates an object of each class to interact with each other
+    public void classInteraction(){
 
+    }
     //4: begin and end the game
+    public void startAndGameOver(){
 
+    }
     //5: asks for inputs from the players
-
+    public void playerInput(){
+        
+    }
     //6: displays information to the players (like hit or miss)
+    public void hitOrMiss(){
+
+    }
+
 }
--- src/edu/colorado/applepear/GameTests.java
+++ src/edu/colorado/applepear/GameTests.java
@@ -1,18 +1,18 @@
-package edu.colorado.applepear;
-
-import static org.hamcrest.CoreMatchers.*;
-import static org.hamcrest.MatchersAsser.*;
-
-import org.junit.Before;
-import org.junit.Test;
-
-public class GameTest{
-    Private Game game;
-
-    @Before
-    public void setUp(){
-        game = new Game();
-    }
-
-    @Test
-}
+//package edu.colorado.applepear;
+//
+//import static org.hamcrest.CoreMatchers.*;
+//import static org.hamcrest.MatchersAsser.*;
+//
+//import org.junit.Before;
+//import org.junit.Test;
+//
+//public class GameTest{
+//    Private Game game;
+//
+//    @Before
+//    public void setUp(){
+//        game = new Game();
+//    }
+//
+//    @Test
+//}
