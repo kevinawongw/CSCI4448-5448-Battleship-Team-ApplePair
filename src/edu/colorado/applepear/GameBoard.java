b2c13422b729f3810416cc5eddad3be7fa7a3f01
kevinawongw <kevinawongw@gmail.com>
2021-02-17 01:50:33 -0700

    Team ApplePair pair 1: Kevina push - GameBoard and Main - Ship input added, ship map added, update attack map

--- src/edu/colorado/applepear/GameBoard.java
+++ src/edu/colorado/applepear/GameBoard.java
@@ -1,46 +1,362 @@
 package edu.colorado.applepear;
+import java.util.Scanner;
 
 // Kevina did this section
+
+import java.util.Scanner;
 
 public class GameBoard {
 
     // Helpful Variables
-    public static int numX = 10;
-    public static int numY = 10;
+    public static final int numX = 10;
+    public static final int numY = 10;
 
+    // Initializing Maps:
 
+    // Attack Map: Map reflects what coordinates have been attacked as well as the results of those attacks
     // Note from Kevina: For now, I'm going to make the map 0,1,2 for y/n ship. We can change to string if easier :)
     // 0 - unchecked
     // 1 - checked, no ship
     // 2 - checked, ship found
+    public int[][] attackMap;
 
-    public static int[][] map = new int [numX][numY];
+    // Ship Map: Reflects that player's own ships
+    // 0 - Not Present
+    // 1 - Present
+    private int[][] shipMap;
 
 
-    
+    private Player p;
+
+    public GameBoard(Player p){
+        this.p = p;
+
+        shipMap = new int[numX][numY];
+        attackMap = new int [numX][numY];
+    }
+
+    public Player getPlayer() {
+        return p;
+    }
+
+    // Getter
+    public int[][] getShipMap(){
+        return this.shipMap;
+    }
+
+
     // Function 2: View Map
     // Param: None
     // Returns: None
     // Prints Map with 0s, 1s, and 2s
-    public static void viewMap() {
+    public void viewMap() {
+        System.out.print("----- " + getPlayer().getName() + "'s Map -----\n\n");
+
         for (int i = 0; i < numX; i++){
+            System.out.print("  ");
+
             for (int a = 0 ; a < 10 ; a++){
-                System.out.print("+———");
+                if (i == 0){
+                    System.out.print("+—" + a + "—");
+                }
+                else {
+                    System.out.print("+———");
+                }
             }
             System.out.print("+");
             System.out.print("\n");
-            System.out.print("| ");
+            System.out.print(i + " | ");
 
             for (int j = 0; j < numY; j++){
-                System.out.print(map[i][j] + " | ");
+                if (attackMap[i][j] == 1){
+                    System.out.print("M" + " | ");
+
+                }
+                else if (attackMap[i][j] ==2){
+                    System.out.print("H" + " | ");
+
+                }
+                else {
+                    System.out.print(attackMap[i][j] + " | ");
+                }
             }
             System.out.print("\n");
         }
+        System.out.print("  ");
         for (int a = 0 ; a < 10 ; a++){
             System.out.print("+———");
         }
-        System.out.print("+");
+        System.out.print("+\n\n");
     }
 
 
+    // Function 3: View Ship Map
+    // Param: None
+    // Returns: None
+    // Prints Map with 0s, 1s
+    // Prints where that player's ships are located
+    public void viewShips() {
+        System.out.print("----- " + getPlayer().getName() + "'s Ships -----\n\n");
+
+        for (int i = 0; i < numX; i++){
+            System.out.print("  ");
+
+            for (int a = 0 ; a < 10 ; a++){
+                if (i == 0){
+                    System.out.print("+—" + a + "—");
+                }
+                else {
+                    System.out.print("+———");
+                }
+            }
+            System.out.print("+");
+            System.out.print("\n");
+            System.out.print(i + " | ");
+
+            for (int j = 0; j < numY; j++){
+                System.out.print(shipMap[i][j] + " | ");
+            }
+            System.out.print("\n");
+        }
+        System.out.print("  ");
+        for (int a = 0 ; a < 10 ; a++){
+            System.out.print("+———");
+        }
+        System.out.print("+\n\n");
+    }
+
+    // Function 4: Populate Ships - ShipMap Setter
+    // Param: None - takes in terminal input
+    // Returns: None - Directly modifies shipMap
+    // Pseudocode:
+    // For i in numShips (5)
+    // Ask user for rotation preference (except for "Tower" and "L")
+    // Ask user for top-most/right-most block for ship placement (depending on rotation preference)
+    // Check to make sure user input coordinates are unfilled
+    // If unfilled (if 0)
+    // Fill coordinates in shipMap with 1 to indicate ship presence
+    // Repeat with all 5 ships
+    // Print Ships
+
+    // --Ships--:
+    // Minesweeper (2 blocks)
+    // Destroyer (3 blocks)
+    // Battleship (4 Blocks)
+    // Tower (1 block x 3 hits)
+    // L - Ship (3 blocks shaped like "L")
+    public void populateShips() {
+        Scanner myInput = new Scanner(System.in);
+        for (int i = 1; i < 6; i++) {
+            String input = null;
+            if (i == 1) {
+                System.out.println("Place Minesweeper (2 blocks wide): ");
+                System.out.println("Enter \"1\" or \"2\" \n 1. Horizontal \n 2. Vertical ");
+                boolean temp = true;
+                while (temp) {
+                    input = myInput.nextLine();
+                    if (input.equals("1") | input.equals("2")) {
+                        temp = false;
+                    } else {
+                        System.out.println("Input must be \"1\" or \"2\"! ");
+                    }
+                }
+                if (input.equals("1")) {
+                    System.out.println("Enter the X-coordinate of the right-most block of your ship: ");
+                    String inputValX = myInput.nextLine();
+                    System.out.println("Enter the Y-coordinate of the right-most block of your ship: ");
+                    String inputValY = myInput.nextLine();
+
+                    shipMap[Integer.parseInt(inputValY)][Integer.parseInt(inputValX)] = 1;
+                    shipMap[Integer.parseInt(inputValY)][Integer.parseInt(inputValX) + 1] = 1;
+                    viewShips();
+
+
+                } else if (input.equals("2")) {
+                    System.out.println("Enter the X-coordinate of the top-most block of your ship: ");
+                    String inputValX = myInput.nextLine();
+                    System.out.println("Enter the Y-coordinate of the top-most block of your ship: ");
+                    String inputValY = myInput.nextLine();
+
+                    shipMap[Integer.parseInt(inputValY)][Integer.parseInt(inputValX)] = 1;
+                    shipMap[Integer.parseInt(inputValY) + 1][Integer.parseInt(inputValX)] = 1;
+                    viewShips();
+
+
+                }
+            } else if (i == 2) {
+                String input2 = null;
+                System.out.println("Enter \"1\" or \"2\" \n 1. Horizontal \n 2. Vertical ");
+                System.out.println("Place Destroyer (3 blocks wide): ");
+                boolean temp = true;
+                while (temp) {
+                    input2 = myInput.nextLine();
+                    if (input2.equals("1") | input2.equals("2")) {
+                        temp = false;
+                    } else {
+                        System.out.println("Input must be \"1\" or \"2\"! ");
+                    }
+                }
+                if (input2.equals("1")) {
+                    temp = true;
+                    while (temp) {
+                        System.out.println("Enter the X-coordinate of the right-most block of your ship: ");
+                        String inputValX = myInput.nextLine();
+                        System.out.println("Enter the Y-coordinate of the right-most block of your ship: ");
+                        String inputValY = myInput.nextLine();
+                        boolean criteriaA = (shipMap[Integer.parseInt(inputValY)][Integer.parseInt(inputValX)] == 0);
+                        boolean criteriaB = (shipMap[Integer.parseInt(inputValY)][Integer.parseInt(inputValX) + 1] == 0);
+                        boolean criteriaC = (shipMap[Integer.parseInt(inputValY)][Integer.parseInt(inputValX) + 2] == 0);
+
+                        if (criteriaA && criteriaB && criteriaC) {
+                            shipMap[Integer.parseInt(inputValY)][Integer.parseInt(inputValX)] = 1;
+                            shipMap[Integer.parseInt(inputValY)][Integer.parseInt(inputValX) + 1] = 1;
+                            shipMap[Integer.parseInt(inputValY)][Integer.parseInt(inputValX) + 2] = 1;
+                            temp = false;
+                            viewShips();
+
+                        } else {
+                            System.out.println("You already placed another ship there! Try another location.");
+                        }
+
+                    }
+                } else if (input2.equals("2")) {
+                    temp = true;
+                    while (temp) {
+                        System.out.println("Enter the X-coordinate of the top-most block of your ship: ");
+                        String inputValX = myInput.nextLine();
+                        System.out.println("Enter the Y-coordinate of the top-most block of your ship: ");
+                        String inputValY = myInput.nextLine();
+                        boolean criteriaA = (shipMap[Integer.parseInt(inputValY)][Integer.parseInt(inputValX)] == 0);
+                        boolean criteriaB = (shipMap[Integer.parseInt(inputValY) + 1][Integer.parseInt(inputValX)] == 0);
+                        boolean criteriaC = (shipMap[Integer.parseInt(inputValY) + 2][Integer.parseInt(inputValX)] == 0);
+
+                        if (criteriaA && criteriaB && criteriaC) {
+                            shipMap[Integer.parseInt(inputValY)][Integer.parseInt(inputValX)] = 1;
+                            shipMap[Integer.parseInt(inputValY) + 1][Integer.parseInt(inputValX)] = 1;
+                            shipMap[Integer.parseInt(inputValY) + 2][Integer.parseInt(inputValX)] = 1;
+                            temp = false;
+                            viewShips();
+
+                        } else {
+                            System.out.println("You already placed another ship there! Try another location.");
+                        }
+
+                    }
+
+                }
+            } else if (i == 3) {
+                String input3 = null;
+                System.out.println("Place Destroyer (3 blocks wide): ");
+                System.out.println("Enter \"1\" or \"2\" \n 1. Horizontal \n 2. Vertical ");
+
+                boolean temp = true;
+                while (temp) {
+                    input3 = myInput.nextLine();
+                    if (input3.equals("1") | input3.equals("2")) {
+                        temp = false;
+                    } else {
+                        System.out.println("Input must be \"1\" or \"2\"! ");
+                    }
+                }
+                if (input3.equals("1")) {
+                    temp = true;
+                    while (temp) {
+                        System.out.println("Enter the X-coordinate of the right-most block of your ship: ");
+                        String inputValX = myInput.nextLine();
+                        System.out.println("Enter the Y-coordinate of the right-most block of your ship: ");
+                        String inputValY = myInput.nextLine();
+                        boolean criteriaA = (shipMap[Integer.parseInt(inputValY)][Integer.parseInt(inputValX)] == 0);
+                        boolean criteriaB = (shipMap[Integer.parseInt(inputValY)][Integer.parseInt(inputValX) + 1] == 0);
+                        boolean criteriaC = (shipMap[Integer.parseInt(inputValY)][Integer.parseInt(inputValX) + 2] == 0);
+                        boolean criteriaD = (shipMap[Integer.parseInt(inputValY)][Integer.parseInt(inputValX) + 3] == 0);
+
+                        if (criteriaA && criteriaB && criteriaC && criteriaD) {
+                            shipMap[Integer.parseInt(inputValY)][Integer.parseInt(inputValX)] = 1;
+                            shipMap[Integer.parseInt(inputValY)][Integer.parseInt(inputValX) + 1] = 1;
+                            shipMap[Integer.parseInt(inputValY)][Integer.parseInt(inputValX) + 2] = 1;
+                            shipMap[Integer.parseInt(inputValY)][Integer.parseInt(inputValX) + 3] = 1;
+                            temp = false;
+                            viewShips();
+
+
+                        } else {
+                            System.out.println("You already placed another ship there! Try another location.");
+                        }
+
+                    }
+                } else if (input3.equals("2")) {
+                    temp = true;
+
+                    while (temp) {
+                        System.out.println("Enter the X-coordinate of the top-most block of your ship: ");
+                        String inputValX = myInput.nextLine();
+                        System.out.println("Enter the Y-coordinate of the top-most block of your ship: ");
+                        String inputValY = myInput.nextLine();
+                        boolean criteriaA = (shipMap[Integer.parseInt(inputValY)][Integer.parseInt(inputValX)] == 0);
+                        boolean criteriaB = (shipMap[Integer.parseInt(inputValY) + 1][Integer.parseInt(inputValX)] == 0);
+                        boolean criteriaC = (shipMap[Integer.parseInt(inputValY) + 2][Integer.parseInt(inputValX)] == 0);
+                        boolean criteriaD = (shipMap[Integer.parseInt(inputValY) + 3][Integer.parseInt(inputValX)] == 0);
+
+                        if (criteriaA && criteriaB && criteriaC && criteriaD) {
+                            shipMap[Integer.parseInt(inputValY)][Integer.parseInt(inputValX)] = 1;
+                            shipMap[Integer.parseInt(inputValY) + 1][Integer.parseInt(inputValX)] = 1;
+                            shipMap[Integer.parseInt(inputValY) + 2][Integer.parseInt(inputValX)] = 1;
+                            shipMap[Integer.parseInt(inputValY) + 3][Integer.parseInt(inputValX)] = 1;
+                            viewShips();
+                            temp = false;
+                        } else {
+                            System.out.println("You already placed another ship there! Try another location.");
+                        }
+
+                    }
+
+                }
+
+            } else if (i == 4) {
+                System.out.println("Place Tower (1): ");
+                boolean temp = true;
+                while (temp) {
+                    System.out.println("Enter the X-coordinate of the ship: ");
+                    String inputValX = myInput.nextLine();
+                    System.out.println("Enter the Y-coordinate of the ship: ");
+                    String inputValY = myInput.nextLine();
+                    boolean criteriaA = (shipMap[Integer.parseInt(inputValY)][Integer.parseInt(inputValX)] == 0);
+
+                    if (criteriaA) {
+                        shipMap[Integer.parseInt(inputValY)][Integer.parseInt(inputValX)] = 1;
+                        temp = false;
+                        viewShips();
+
+                    } else {
+                        System.out.println("You already placed another ship there! Try another location.");
+                    }
+                }
+            } else if (i == 5) {
+                System.out.println("Place L-Ship (3 blocks is L shape): ");
+                boolean temp = true;
+                while (temp) {
+                    System.out.println("Enter the X-coordinate of the top left-most block of your ship: ");
+                    String inputValX = myInput.nextLine();
+                    System.out.println("Enter the Y-coordinate of the top left-most block of your ship: ");
+                    String inputValY = myInput.nextLine();
+                    boolean criteriaA = (shipMap[Integer.parseInt(inputValY)][Integer.parseInt(inputValX)] == 0);
+                    boolean criteriaB = (shipMap[Integer.parseInt(inputValY) + 1][Integer.parseInt(inputValX)] == 0);
+                    boolean criteriaC = (shipMap[Integer.parseInt(inputValY) + 1][Integer.parseInt(inputValX) + 1] == 0);
+
+                    if (criteriaA && criteriaB && criteriaC) {
+                        shipMap[Integer.parseInt(inputValY)][Integer.parseInt(inputValX)] = 1;
+                        shipMap[Integer.parseInt(inputValY) + 1][Integer.parseInt(inputValX)] = 1;
+                        shipMap[Integer.parseInt(inputValY) + 1][Integer.parseInt(inputValX) + 1] = 1;
+                        temp = false;
+                        viewShips();
+
+                    } else {
+                        System.out.println("You already placed another ship there! Try another location.");
+                    }
+                }
+            }
+            viewShips();
+        }
+    }
 }
+
--- src/edu/colorado/applepear/Main.java
+++ src/edu/colorado/applepear/Main.java
@@ -1,44 +1,68 @@
 package edu.colorado.applepear;
 import java.util.Scanner;
+
 // Kevina was here doing menu things
 
 public class Main {
 
-
+    // Variable to keep game running
     private static boolean run = true;
 
+    // Helper Function - Display Menu - Kevina
     public static void displayMenu() {
-        System.out.println("\n\n ========== Welcome to Battleship ========== \n");
+        System.out.println("\n\n ========== Battleship Menu ========== \n");
         System.out.println("Please pick a menu option");
         System.out.println("1. Choose Coordinates to attack");
         System.out.println("2. View Current Map");
         System.out.println("3. Check Player Inventory");
         System.out.println("4. Help & Instructions");
-        System.out.println("5. Quit >:(");
+        System.out.println("5. View My Ships");
+        System.out.println("6. Quit >:(");
 
     }
 
+    // Main - Kevina
     public static void main(String[] args) {
-        // write your code here
-         Ship ship = new Ship();
-         ship.show();
+         // -- BEGIN Initialization --
 
-         GameBoard p1Map = new GameBoard();
-         GameBoard p2Map = new GameBoard();
+         // Takes User Inputs (Credit: GeeksForGeeks)
+         Scanner myInput = new Scanner(System.in);
 
+         // Initialize Objects for Game + Take User Inputs for Object Parameters - Kevina
+
+         // Initialize Player Objects
+         System.out.println("\n\n ========== Welcome to Battleship ========== \n");
+         System.out.println("\n Enter Player 1's Name: \n");
+         String name1 = myInput.nextLine();
+         System.out.println("\n Enter Player 2's Name: \n");
+         String name2 = myInput.nextLine();
+         Player p1 = new Player(name1,2,2,5);
+         Player p2 = new Player(name2,2,2,5);
+
+         // Initialize GameBoard Objects
+         GameBoard p1Map = new GameBoard(p1);
+         GameBoard p2Map = new GameBoard(p2);
+         System.out.println("\n\n ========== " + p1Map.getPlayer().getName() + "'s Turn to Place Ships ========== \n");
+         p1Map.populateShips();
+         System.out.println("\n\n ========== " + p2Map.getPlayer().getName() + "'s Turn to Place Ships ========== \n");
+         p2Map.populateShips();
+
+         // Initialize Game Object
+         Game myGame = new Game (p1,p2,p1Map, p2Map);
+
+         // Variables to keep track of which player's turn
          GameBoard curMap = p1Map;
          GameBoard oppMap = p2Map;
 
-//         Player p1 = new Player();
-//         Player p2 = new Player();
+         // -- END Initialization--
 
-         // Kevina was here
+         // -- BEGIN Game Menu Loop --
+
          // Menu Display & User Input
          // Refer to the displayMenu Print function for what each menu option will do
-
          displayMenu();
-         while (run == true ){
-             Scanner myInput = new Scanner(System.in);
+
+         while (run){
              String myVal = myInput.nextLine();
              System.out.println(myVal);
              if (myVal.equals("1")){
@@ -46,11 +70,27 @@ public class Main {
                  String myX = myInput.nextLine();
                  System.out.println("What is the Y coordinate for the space you want to attack?");
                  String myY = myInput.nextLine();
-                 Game.updateMap(curMap, myX,myY);
-                 //... INCOMPLETE -
-                 // Update Player map of reflected attack and new accessed space
-                 // Update Missiles if specialty missile was used
-                 // Update user of hit or miss
+                 boolean hitOrMiss = Game.updateMap(curMap,oppMap, myX,myY);
+                 if (hitOrMiss){
+                     System.out.println("You Hit an Opponent's Ship! Nice Shot!");
+                 }
+                 else{
+                     System.out.println("You Missed...");
+                 }
+                 curMap.viewMap();
+                 // Depending on the player's turn, their map will be different
+                 // Update maps and turns
+                 if (curMap.equals(p1Map)){
+                     p1Map = curMap;
+                     curMap = p2Map;
+                     oppMap = p1Map;
+
+                 }
+                 else if (curMap.equals(p2Map)){
+                     p2Map = curMap;
+                     curMap = p1Map;
+                     oppMap = p2Map;
+                 }
              }
              else if (myVal.equals("2")){
                  System.out.println("Player Map");
@@ -63,25 +103,16 @@ public class Main {
 
              }
              else if (myVal.equals("5")){
+                 curMap.viewShips();
+             }
+             else if (myVal.equals("6")){
                 run = false;
                 return;
              }
              displayMenu();
-
-             // Depending on the player's turn, their map will be different
-             // Update maps and turns
-             if (curMap.equals(p1Map)){
-                 curMap = p2Map;
-                 oppMap = p1Map;
-
-             }
-             else if (curMap.equals(p2Map)){
-                 curMap = p1Map;
-                 oppMap = p2Map;
-             }
         }
-
     }
+    // -- END Game Menu Loop--
 
 
 
