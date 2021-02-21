f87f23b7631551fdb00f34015487b53366df3cb2
viwo3022 <viwo3022@colorado.edu>
2021-02-20 22:04:03 -0700

    Team ApplePair pair 1: Vienna push - modified GameTest

--- src/edu/colorado/applepear/GameTest.java
+++ src/edu/colorado/applepear/GameTest.java
@@ -14,6 +14,7 @@ public class GameTest {
 
     @BeforeEach
     public void setUp() throws Exception {
+        System.out.println("Before Each Test");
         player1 = new Player("vienna", 3, 3, 5);
         player2 = new Player("kevina", 3, 3, 5);
     }
@@ -21,27 +22,31 @@ public class GameTest {
     @Test
     @DisplayName("Displaying player1 name")
     public void testGetName() {
+        System.out.println("Testing GetName");
         assertEquals("vienna", player1.getName(),
                 "GetName should display player name");
         assertEquals("kevina", player2.getName(),
                 "GetName should display player name");
     }
-
+    @Test
     @DisplayName("Getting number of ships")
     public void testGetNumShips() {
+        System.out.println("Testing GetNumShips");
         assertEquals(5, player1.getNumShips(),
                 "GetNumShips should give the number of ships");
     }
-
+    @Test
     @DisplayName("Getting number of missiles")
     public void testGetNumRadarMissiles() {
+        System.out.println("Testing GetRadarMissiles");
         assertEquals(3, player1.getRadarMissile(),
                 "GetRadarMissile should give the player's number of radar missiles");
         assertEquals(3, player2.getRadarMissile(),
                 "GetRadarMissile should give the player's number of radar missiles");
     }
-
+    @Test
     public void testGetNumPlusMissiles() {
+        System.out.println("Testing getPlusMissiles");
         assertEquals(3, player1.getPlusMissile(),
                 "GetPlusMissile should give the player's number of Plus missiles");
         assertEquals(3, player2.getPlusMissile(),
