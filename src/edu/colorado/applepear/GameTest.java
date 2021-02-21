985c284d8a4a0c3fae39b0ba1c0064bdebe5772d
vonvonliu <yuli4421@colorado.edu>
2021-02-20 23:44:32 -0700

    Update GameTest.java

--- src/edu/colorado/applepear/GameTest.java
+++ src/edu/colorado/applepear/GameTest.java
@@ -11,42 +11,42 @@ public class GameTest {
 
     private Player player1;
     private Player player2;
-    private Gameboard gb1;
-    private Gameboard gb2;
+    private GameBoard gb1;
+    private GameBoard gb2;
     private Game game;
 
     @BeforeEach
     public void setUp() throws Exception {
         player1 = new Player("vienna", 3, 3, 5);
         player2 = new Player("kevina", 3, 3, 5);
-        gb1 = new Gameboard(player1);
-        gb2 = new Gameboard(player2);
+        gb1 = new GameBoard(player1);
+        gb2 = new GameBoard(player2);
         game = new Game(player1, player2, gb1, gb2);
     }
 
     @Test
     @DisplayName("if updateMap and hit a ship")
     public void testUpdateMap1() {
-            assertEquals(true, , "updateMap should return true");
+            assertEquals(true, game.updateMap(), "updateMap should return true");
     }
 
     @Test
     @DisplayName("if updateMap and did not hit a ship")
     public void testUpdateMap2() {
-            assertEquals(false, , "updateMap should return false");
+            assertEquals(false, game.updateMap(), "updateMap should return false");
     }
 
     @Test
     @DisplayName("if game is not over")
     public void testIsGameOver1() {
             p1.checkLose() = true;
-            assertEquals(true, , "isGameOver should return true");
+            assertEquals(true, game.isGameOver(), "isGameOver should return true");
     }
 
     @Test
     @DisplayName("if game is over")
     public void testIsGameOver(2) {
-            assertEquals(false, , "isGameOver should return false");
+            assertEquals(false, game.isGameOver(), "isGameOver should return false");
     }
 
 }
