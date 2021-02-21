20b08675e34a2bd77fd1447f3a4617c9428ad680
Yubin Go(Sally) <54964332+yugo9081@users.noreply.github.com>
2021-02-21 15:23:47 +0900

    Team applepear pair 2: Processing on ShipTest.java 2

--- src/edu/colorado/applepear/ShipTest.java
+++ src/edu/colorado/applepear/ShipTest.java
@@ -8,10 +8,26 @@ import org.junit.jupiter.api.Test;
 
 public class ShipTest {
     private Ship ship;
+    private GameBoard gb;
+    private Player p;
+    private Point p1;
+    private Point p2;
 
     @BeforeEach
     public void setShip() throws Exception{
         System.out.println("Before Each Test");
         ship = new Ship("minesweeper");
+        p = new Player("yubin", 3,3,5);
+        gb = new GameBoard(p);
+        p1 = new Point(1,2);
+        p2 = new Point(1,1);
+        gb.placeMinesweeper(p1,p2);
+    }
+
+    @Test
+    @DisplayName("Displaying ship type")
+    public void testGetShip(){
+        System.out.println("Testing GetShip");
+        assertEquals("minesweeper",gb.minesweeper.getShipType(),"GetShip should display the minesweeper");
     }
 }
