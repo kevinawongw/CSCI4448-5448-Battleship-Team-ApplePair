b793f2724dde5c8f1036f6010bd2334ab035f57f
viwo3022 <viwo3022@colorado.edu>
2021-03-03 21:32:30 -0700

    Team Applepear Pair 1: Vienna adds radar missile feature

--- src/edu/colorado/applepear/Player.java
+++ src/edu/colorado/applepear/Player.java
@@ -1,17 +1,22 @@
 package edu.colorado.applepear;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 //Vienna part: player class
 public class Player{
     // player class will hold the following
     private String name;
     private int radarMissile, plusMissile, numShips;
     private GameBoard gb;
+
     //constructor
     public Player(String name, GameBoard gb){
         this.name = name;
         this.radarMissile = 3;
         this.plusMissile = 3;
-        gb = gb;
+
     }
 
     //setters
@@ -35,6 +40,56 @@ public class Player{
     public int getPlusMissile() { return plusMissile; }
 
     public GameBoard getGb() { return gb; }
+
+    public boolean radarMissile( GameBoard opponentBoard, Point P){
+        if (getRadarMissile() < 1){
+            System.out.println( getName() + "has no more radar missiles remaining");
+            return false;
+        }
+        else{
+            //update number of radar missiles for this player
+            setRadarMissile(getRadarMissile()-1);
+            System.out.println("Using radar missile...");
+            //points
+
+            Point above = new Point(P.getX(),P.getY()+1);
+            Point below = new Point(P.getX(),P.getY()-1);
+            Point right = new Point(P.getX()+1,P.getY()+1);
+            Point left = new Point(P.getX()-1,P.getY()+1);
+            Point upRight = new Point(P.getX()+1,P.getY()+1);
+            Point upLeft = new Point(P.getX()-1,P.getY()+1);
+            Point downRight = new Point(P.getX()+1,P.getY()-1);
+            Point downLeft = new Point(P.getX()-1,P.getY()-1);
+
+            List<Point> radarRange = new ArrayList<>();
+
+            radarRange.add(P);
+            radarRange.add(above);
+            radarRange.add(below);
+            radarRange.add(right);
+            radarRange.add(left);
+            radarRange.add(upRight);
+            radarRange.add(upLeft);
+            radarRange.add(downRight);
+            radarRange.add(downLeft);
+
+            int[][] map =  opponentBoard.getShipMap();
+
+            for ( Point i :radarRange){
+                int thisX = i.getX();
+                int thisY = i.getY();
+                if (map[thisX][thisY] == 1){
+                    System.out.println("Ship detected in this range!");
+                    return true;
+                }
+            }
+            System.out.println("No ship was found in this range");
+            return false;
+
+        }
+
+    }
+
 
 }
 
--- src/edu/colorado/applepear/PlayerTest.java
+++ src/edu/colorado/applepear/PlayerTest.java
@@ -7,6 +7,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PlayerTest {
 
     Player player1;
@@ -17,9 +20,23 @@ public class PlayerTest {
 
     @BeforeEach
     public void setUp() {
-        System.out.println("Before Each Test");
+//        System.out.println("Before Each
+
+        g1 = new GameBoard();
+        g2 = new GameBoard();
         player1 = new Player("vienna", g1);
         player2 = new Player("kevina", g2);
+
+
+        Point point1 = new Point(1,1);
+        Point point2 = new Point(1,2);
+
+        List<Point> myPoints = new ArrayList<>();
+        myPoints.add(point1);
+        myPoints.add(point2);
+
+        g2.placeShip(myPoints);
+
     }
 
     @Test
@@ -52,6 +69,16 @@ public class PlayerTest {
         System.out.println("Testing getGb");
         assertEquals(3, player1.getPlusMissile(),
                 "GetPlusMissile should give the player's number of Plus missiles");
+
+    }
+
+    @Test
+    public void radarMissileTest(){
+        System.out.println("Testing radarMissile");
+        Point testPoint = new Point( 1,0);
+        assertTrue(player1.radarMissile(g2, testPoint),
+                "radarMissile should return true");
+
 
     }
 
