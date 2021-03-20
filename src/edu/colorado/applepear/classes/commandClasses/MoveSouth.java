dfebb7d6a17d9ab59af35968cda4fc8df1f9c127
Yubin Go(Sally) <54964332+yugo9081@users.noreply.github.com>
2021-03-21 00:55:03 +0900

    Add the MoveSouth.java

--- src/edu/colorado/applepear/classes/commandClasses/MoveSouth.java
+++ src/edu/colorado/applepear/classes/commandClasses/MoveSouth.java
@@ -1,4 +1,43 @@
 package edu.colorado.applepear.classes.commandClasses;
+import edu.colorado.applepear.classes.Command;
+import edu.colorado.applepear.classes.GameBoard;
+import edu.colorado.applepear.classes.Point;
+import edu.colorado.applepear.classes.Ship;
 
-public class MoveSouth {
+import java.util.*;
+
+public class MoveSouth implements Command{
+    GameBoard gb;
+    Ship ship;
+
+    public MoveSouth(GameBoard gb, Ship ship){
+        this.gb = gb;
+        this.ship = ship;
+    }
+
+    @Override
+    public void execute() {
+        List <String> valid = gb.getPossibleMoveLocations(ship);
+        List <Point> newLocations = new ArrayList<>();
+        if(valid.contains("South")){
+            if(!ship.getUnderwater()){
+                for(Point p : ship.getLocation()){
+                    newLocations.add(new Point(p.getX(), p.getY() - 1));
+                    gb.setShipMap(p, 0);
+                }
+                ship.setLocation(newLocations);
+                gb.updateShipMap();
+                gb.viewShips();
+            }
+            else if(ship.getUnderwater()){
+                for (Point p : ship.getLocation()){
+                    newLocations.add(new Point(p.getX(), p.getY()-1));
+                    gb.setUnderwaterMap(p,0);
+                }
+                ship.setLocation(newLocations);
+                gb.updateUnderwaterMap();
+                gb.viewUnderwater();
+            }
+        }
+    }
 }
