7fa4805f52c8cec54d326834293eaaad1f836b22
vonvonliu <yuli4421@colorado.edu>
2021-03-20 11:23:03 -0600

    Move East and Move West

--- src/edu/colorado/applepear/classes/commandClasses/MoveWest.java
+++ src/edu/colorado/applepear/classes/commandClasses/MoveWest.java
@@ -1,4 +1,41 @@
 package edu.colorado.applepear.classes.commandClasses;
+import edu.colorado.applepear.classes.GameBoard;
+import edu.colorado.applepear.classes.Point;
+import edu.colorado.applepear.classes.Ship;
 
-public class MoveWest {
+import java.util.*;
+
+public class MoveWest implements Command{
+    GameBoard gb;
+    Ship ship;
+
+    public MoveWest(GameBoard gb, Ship ship){
+        this.gb = gb;
+        this.ship = ship;
+    }
+    @Override
+    public void execute() {
+        List <String> valid = gb.getPossibleMoveLocations(ship);
+        List <Point> newLocations = new ArrayList<>();
+        if(valid.contains("West")){
+            if(!ship.getUnderwater()){
+                for(Point p : ship.getLocation()){
+                    newLocations.add(new Point(p.getX(), p.getY() - 1));
+                    gb.setShipMap(p, 0);
+                }
+                ship.setLocation(newLocations);
+                gb.updateShipMap();
+                gb.viewShips();
+            }
+            else if(ship.getUnderwater()){
+                for (Point p : ship.getLocation()){
+                    newLocations.add(new Point(p.getX(), p.getY()-1));
+                    gb.setUnderwaterMap(p,0);
+                }
+                ship.setLocation(newLocations);
+                gb.updateUnderwaterMap();
+                gb.viewUnderwater();
+            }
+        }
+    }
 }
