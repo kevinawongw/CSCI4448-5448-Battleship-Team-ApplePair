539e0c206a9febafbc8f644f0c0435e736a15523
vonvonliu <yuli4421@colorado.edu>
2021-03-21 09:42:38 -0600

    Fixed bug in undo

--- src/edu/colorado/applepear/classes/commandClasses/MoveNorth.java
+++ src/edu/colorado/applepear/classes/commandClasses/MoveNorth.java
@@ -46,22 +46,22 @@ public class MoveNorth implements Command {
     public void undo(){
         List <Point> newLocations = new ArrayList<>();
         if(!ship.getUnderwater()){
-            for(Point p : ship.getLocation()){
-                newLocations.add(new Point(p.getX(), p.getY() - 1));
+            for (Point p : ship.getLocation()) {
+                newLocations.add(new Point(p.getX(), p.getY() + 1));
                 gb.setShipMap(p, 0);
             }
             ship.setLocation(newLocations);
             gb.updateShipMap();
-//            gb.viewShips();
+            gb.viewShips();
         }
         else if(ship.getUnderwater()){
             for (Point p : ship.getLocation()){
-                newLocations.add(new Point(p.getX(), p.getY() - 1));
+                newLocations.add(new Point(p.getX(), p.getY() + 1));
                 gb.setUnderwaterMap(p,0);
             }
             ship.setLocation(newLocations);
             gb.updateUnderwaterMap();
-//            gb.viewUnderwater();
+            gb.viewUnderwater();
         }
     }
 
