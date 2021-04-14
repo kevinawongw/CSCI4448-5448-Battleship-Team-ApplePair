8ad318ed118de232342f4a189bb53b1d1c419268
Yubin Go(Sally) <54964332+yugo9081@users.noreply.github.com>
2021-04-15 02:16:52 +0900

    Delete Move

--- src/edu/colorado/applepear/classes/GUI/PlayerGUI.java
+++ src/edu/colorado/applepear/classes/GUI/PlayerGUI.java
@@ -1,9 +1,19 @@
 package edu.colorado.applepear.classes.GUI;
 
 import javax.swing.*;
+import java.awt.color.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class PlayerGUI {
+
+public class PlayerGUI extends JFrame implements ActionListener {
     private JPanel ScreenMain;
     private JTextField p2Name;
     private JTextField p1Name;
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }
